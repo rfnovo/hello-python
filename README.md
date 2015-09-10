268640e7f90857e97eacca6d5ca621eba73868c7
Rafael <rafael.novo@emc.com>
2015-09-10 12:12:36 -0300

    REDIS Updated

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Sample Python Web application
-==``===========================
+=============================
 
 The`` sample is using [Flask microframework](http://flask.pocoo.org/) and is intented to test the Python support on [Pivotal's Cloud Foundry](https://run.pivotal.io/).
 `````
@@ -16,3 +16,9 @@ or
 ```script
 cf push <YOUR_APP_NAME> -m 128M
 ````
+UPDATE: Deploy with REDIS
+```script
+cf push hello-world-rafa-counter -m 128M
+cf bind-service hello-world-rafa-counterRedisCloud
+cf restage hello-world-rafa-counter
+```
--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 import os
 import uuid
-from flask import Flask
+import redis
+import json
+from flask import Flask, session
 
 
 app = Flask(__name__)
@@ -10,23 +12,37 @@ GREEN = "#33CC33"
 
 COLOR = GREEN
 
+
+rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
+credentials = rediscloud_service['credentials']
+r = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
+
+r.set("rafa_hit_counter", 1)
+
+def sumhitcounter():
+    r.incr("rafa_hit_counter")
+    pcounter = r.get("rafa_hit_counter")
+    return pcounter
+
+
 @app.route('/')
 def hello():
-
-
+#    sumhitcounter()
     return """
     <html>
     <body bgcolor="{}">
 
     <center><h1><font color="white">Hi, I'm GUID:<br/>
     {}</br>
-
+    <hr>
+    <h2>Page Counter: {}
 
     </center>
 
     </body>
     </html>
-    """.format(COLOR,my_uuid,)
+    """.format(COLOR,my_uuid,sumhitcounter(),)
+    
 
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
